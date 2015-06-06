c17909039b722af9fad172294bfc80f4a403d2fa
Tomi Novak <tominovak33@gmail.com>
2015-06-06 11:51:24 +0100

    Add node.js api to send and recieve posts
    Allows the GET or POST connections to /api/posts to either send
    or recieve a post in json format
    
    Also includes a snippets folder where various useful snippets of
    code are stored that are not used by the application itself but
    can be very useful during development. (Eg: a pre written curl request
    to test the API)

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', function(request, response) {
-	response.status(200);
-	response.send("Express server working correctly");
+var app = express();
+app.use(bodyParser.json());
+
+app.get('/api/posts', function (request, response) {
+	response.json([
+		{
+			username: 'tomi4',
+			body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit one.'
+		}
+	])
 })
 
-app.listen(8888);
+app.post('/api/posts', function (request, response) {
+	console.log('Post recieved');
+	console.log(request.body.username);
+	console.log(request.body.body);
+	response.status(201);
+	response.send();
+})
+
+var port =  (process.argv[2] || 3000);
+
+app.listen(port, function () {
+	console.log('Server listening on port: ', port);
+})
---
+++ snippets/snippets.md
@@ -0,0 +1,10 @@
+Tomi: MEAN practice app 
+========================
+
+### Userful snippets
+
+### Testing the api
+
+* Post data to the API:
+
+curl -v -H "Content-Type: application/json" -XPOST --data "{\"username\":\"tomi5\", \"body\": \"Lorem ipsum dolor sit amet, consectetur adipiscing elit post\"}" localhost:3000/api/posts/
