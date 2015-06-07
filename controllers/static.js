e26b49a46a2210ebb9d3f40a67c5521efa03aba1
Tomi Novak <tominovak33@gmail.com>
2015-06-07 14:41:57 +0100

    remove debugging code

--- controllers/static.js
+++ controllers/static.js
@@ -3,7 +3,6 @@ var router = require('express').Router();
 
 router.get('/', function (request, response) {
 	var abs_path = path.resolve(__dirname+'/../layouts/posts.html');
-	console.log(abs_path);
 	response.sendFile(abs_path);
 })
 
