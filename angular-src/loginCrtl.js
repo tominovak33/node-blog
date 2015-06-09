3babe4e66b6d6e36ebdcf6cf32119ed21a8aeb01
Tomi Novak <tominovak33@gmail.com>
2015-06-09 22:37:12 +0100

    Display logged in user
    If a user logs in, bubble up the event to a parent controller
    with $emit, where it is then added to the scope of the application
    controller and displayer int he navbar

--- angular-src/loginCrtl.js
+++ angular-src/loginCrtl.js
@@ -2,8 +2,9 @@ angular.module('app')
 	.controller('LoginCtrl' , ["$scope" , "UserSvc" , function ($scope, UserSvc) {
 		$scope.login = function (username, password) {
 			UserSvc.login(username, password)
-				.then(function (user){
-					console.log(user);
+				.then(function (response){
+					console.log(response.data);
+					$scope.$emit('userLoggedIn', response.data);
 				})
 		}
 	}])
