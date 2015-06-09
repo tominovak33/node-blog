c9527703095f4661fe950f8fb29d608dd8a25676
Tomi Novak <tominovak33@gmail.com>
2015-06-09 21:41:44 +0100

    Fix authentication
    Fix forgot to select username from DB when authenticating
    causing no data to be returned to client

--- controllers/api/sessions.js
+++ controllers/api/sessions.js
@@ -9,22 +9,23 @@ var config = require('../../config');
 router.post('/', function (request, response, next) {
 	User.findOne({username: request.body.username})
 		.select('password')
+		.select('username')
 		.exec(function (error, user) {
-		if (error) { return next(error) }
-		if (!user) {
-			response.status(401);
-			return response.send('Authentication unsuccessful');
-		}
-		bcrypt.compare(request.body.password, user.password, function (error, valid) {
-			if (error) { return next(error) };
-			if (!valid) {
+			if (error) { return next(error) }
+			if (!user) {
 				response.status(401);
 				return response.send('Authentication unsuccessful');
 			}
+			bcrypt.compare(request.body.password, user.password, function (error, valid) {
+				if (error) { return next(error) };
+				if (!valid) {
+					response.status(401);
+					return response.send('Authentication unsuccessful');
+				}
 
-			var token = jwt.encode({username: user.username}, config.secret);
-			response.json(token);
-		})
+				var token = jwt.encode({username: user.username}, config.secret);
+				response.send(token);
+			})
 	})
 })
 
