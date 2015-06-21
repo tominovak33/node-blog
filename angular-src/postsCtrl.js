4791743048e5118838ef4703bc454437dde241e3
Tomi Novak <tominovak33@gmail.com>
2015-06-21 14:07:20 +0100

    only show ckeditor for logged in useres

--- angular-src/postsCtrl.js
+++ angular-src/postsCtrl.js
@@ -17,6 +17,11 @@ angular.module('app')
 			}
 		}
 
+		$scope.init_ckedit = function () {
+		    CKEDITOR.replace('editor1');
+            document.getElementById('post-simple-input').style.display = 'none';
+		}
+
 		$scope.$on('ws:new_post', function(_, post) {
 			$scope.$apply(function () {
 				$scope.posts.unshift(post);
@@ -25,8 +30,7 @@ angular.module('app')
 
 		$scope.$on('$viewContentLoaded', function(){
 			//the page is ready
-            CKEDITOR.replace('editor1');
-            document.getElementById('post-simple-input').style.display = 'none';
+			//alert("foo");
 		});
 
 	PostsService.get()
