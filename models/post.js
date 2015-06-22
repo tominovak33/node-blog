14aabec187f5f8db7a67292d612e73635cb9bc72
Tomi Novak <tominovak33@gmail.com>
2015-06-22 23:43:33 +0100

    allow usernames to be changed

--- models/post.js
+++ models/post.js
@@ -1,7 +1,7 @@
 var db = require('../db');
 
 var mongoose = require('mongoose')
-  , Schema = mongoose.Schema
+var Schema = mongoose.Schema
 
 var Post = db.model('Post', {
 	_author : { type:  Schema.Types.ObjectId, ref: 'User' },
