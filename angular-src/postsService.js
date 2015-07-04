8f7280e309c6f8b1d1388337d4d7dc776fa35f14
Tomi Novak <tominovak33@gmail.com>
2015-07-04 09:16:53 +0100

    work on single posts display controller

--- angular-src/postsService.js
+++ angular-src/postsService.js
@@ -6,4 +6,7 @@ angular.module('app')
 		this.send = function (post) {
 			return $http.post('/api/posts', post);
 		}
+		this.single = function (parameters) {
+			return $http.get('/api/posts', { params: {post_id: parameters.id} } );
+		}
 	}])
--- angular-src/singlePostsCtrl.js
+++ angular-src/singlePostsCtrl.js
@@ -1,9 +1,21 @@
 angular.module('app')
 	.controller('SinglePostCtrl', ["$scope" , "$http", "PostsService", function ($scope, $http, PostsService) {
 
-		alert("Single posts controller loaded");
+		//alert("Single posts controller loaded");
 
 		$scope.$on('$viewContentLoaded', function(){
 			//the page is ready
 		});
+
+					
+		PostsService.single({
+			id: 'foobar-123'
+		})
+		.success(function (post) {
+			/*
+			$scope.post = posts;
+			console.log(post);
+			alert(post);
+			*/
+		})
 	}])
