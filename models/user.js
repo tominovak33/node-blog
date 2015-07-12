d93132fd422a130f28ccfb788df4c4e07549d01a
Tomi Novak <tominovak33@gmail.com>
2015-07-12 22:48:15 +0100

    different idea for user permission levels

--- models/user.js
+++ models/user.js
@@ -6,12 +6,7 @@ var user = db.Schema({
 	first_name: String,
 	last_name: String,
 	bio: String,
-	permissions: {
-    	verified: { type: Boolean, default: false }, //Ability to create posts
-    	editor: { type: Boolean, default: false }, //Everything listed above + ability to edit/delete other users posts
-    	admin: { type: Boolean, default: false }, //Everything listed above + ability to edit/delete other users (including verification)
-    	superuser: { type: Boolean, default: false } //Should be able to do anything that is possible with the system
-  	}
+	permission_level: {type: Number, default: 0}
 })
 
 module.exports = db.model('User', user);
