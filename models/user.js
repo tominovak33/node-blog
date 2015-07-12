5e5e3546c233603b02bb92f22af3a7292c2962b1
Tomi Novak <tominovak33@gmail.com>
2015-07-12 21:55:28 +0100

    add new user permissons to user model

--- models/user.js
+++ models/user.js
@@ -5,7 +5,13 @@ var user = db.Schema({
 	password: {type: String, select: false},
 	first_name: String,
 	last_name: String,
-	bio: String
+	bio: String,
+	permissions: {
+    	verified: { type: Boolean, default: false }, //Ability to create posts
+    	editor: { type: Boolean, default: false }, //Everything listed above + ability to edit/delete other users posts
+    	admin: { type: Boolean, default: false }, //Everything listed above + ability to edit/delete other users (including verification)
+    	superuser: { type: Boolean, default: false } //Should be able to do anything that is possible with the system
+  	}
 })
 
 module.exports = db.model('User', user);
